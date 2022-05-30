d33c7658b06dc67ba60deb9e83d65f5972d97da2
benamos216 <benamos216@gmail.com>
2022-05-30 17:46:29 +0000

    Setup functions for game to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-console.log("test");
+
+
+function runGame() {
+
+}
+
+/**Checks selection made by the user*/
+function checkSelection() {
+
+}
+
+/**Calculates whether the player was correct or not */
+function calculateScore() {
+
+}
+
+/**Increments the players score */
+function playerScore() {
+
+}
+
+/**Increments the computers score */
+function computerScore() {
+
+}
+
+/**Works through the possible options of winning or losing, depending on selection made*/
+
+function displayRock() {
+
+}
+
+function displayPaper() {
+
+}
+
+function displayScissors() {
+
+}
+
+function displayLizard() {
+
+}
+
+function displaySpock() {
+
+}
